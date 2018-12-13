62fa982023884f47640121c1ebfac623ae4a2f6b
Renault <axel.renault1529@gmail.com>
2018-12-13 15:09:18 +0100

    ui

--- Controleur/ControleurCentre.php
+++ Controleur/ControleurCentre.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'Modele/Centre.php';
-require_once 'Vue/Vue.php';
+require_once 'Vue/Centre.php';
 
 class ControleurCentre {
 
--- Controleur/ControleurConnexion.php
+++ Controleur/ControleurConnexion.php
@@ -19,8 +19,9 @@ class ControleurConnexion extends Controleur
 
     public function connecter()
     {
+        
         if ($this->requete->existeParametre("login") && $this->requete->existeParametre("mdp"))
-        {
+        { 
             $login = $this->requete->getParametre("login");
             $mdp = $this->requete->getParametre("mdp");
             if ($this->salarie->connecter($login, $mdp))
@@ -31,10 +32,10 @@ class ControleurConnexion extends Controleur
                 $this->rediriger("admin");
             }
             else
-                $this->genererVue(array('msgErreur' => 'Login ou mot de passe incorrects'),"index");
+                $this->genererVue(array('msgErreur' => 'Login ou mot de passe incorrects'),"index"); 
         }
             //$this->genererVue(array('msgErreur' => 'Login et mot de passe non défini'),'index');
-            throw new Exception("Action impossible : login ou mot de passe non défini");
+            throw new Exception("Action impossible : login ou mot de passe non défini"); 
     }
 
     public function deconnecter()
